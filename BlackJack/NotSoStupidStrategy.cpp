78330f79b90f9a4a5fc3999940a4a4d0bda5f022
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-10-20 19:29:29 +0700

    Update NotSoStupidStrategy.cpp

--- BlackJack/NotSoStupidStrategy.cpp
+++ BlackJack/NotSoStupidStrategy.cpp
@@ -2,6 +2,7 @@
 #include "Factory.h"
 #include <iostream>
 #include <vector>
+#define Pass -1
 
 static bool g(){
     Factory<Strategy, std::string,Strategy*(*)()>::getInstance()->addCreator("NotSoStupid", createNotSoStupidStrategy);
@@ -10,25 +11,29 @@ static bool g(){
 
 static bool b=g();
 
-void NotSoStupidStrategy::printName() {
-    std::cout<<"NotSoStupid"<<std::endl;
-}
-
 Strategy *createNotSoStupidStrategy () {
     return new NotSoStupidStrategy;
 }
 
-int NotSoStupidStrategy::TakeCard() {
+int NotSoStupidStrategy::takeCard() {
     return rand() % 10 + 1;
 }
 
-int NotSoStupidStrategy::Play(int FirstCard) {
+int NotSoStupidStrategy::play(int FirstCard, int FirstCardOfOpponent) {
     std::vector<int> History;
     History.push_back(FirstCard);
     int Sum = FirstCard;
+    if (FirstCardOfOpponent == 10) {
+        for (int i = 0; i < History.size(); ++i) {
+            std::cout<<"History of not so stupid strategy: ";
+            std::cout<<History[i]<< " ";
+            std::cout<<std::endl;
+        }
+        return Pass;
+    }
     int CurrentCard;
     while (Sum <= 18) {
-        CurrentCard = TakeCard();
+        CurrentCard = takeCard();
         History.push_back(CurrentCard);
         Sum += CurrentCard;
     }
