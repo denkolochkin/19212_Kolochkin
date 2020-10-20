a6402e421bc2a1cb1a0f327d53e22589455769a7
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-10-20 18:40:23 +0700

    Create StupidStrategy.h

---
+++ BlackJack/StupidStrategy.h
@@ -0,0 +1,14 @@
+#include "Strategy.h"
+
+
+class StupidStrategy : public Strategy {
+public:
+    void printName() override;
+    ~StupidStrategy(){}
+    int TakeCard() override;
+    int Play(int) override;
+private:
+
+};
+
+Strategy* createStupidStrategy ();
