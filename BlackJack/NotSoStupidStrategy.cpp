7b689bfd93b7b8854e6f8428828308f93dd72ca4
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-10-28 00:47:13 +0700

    Update NotSoStupidStrategy.cpp

--- BlackJack/NotSoStupidStrategy.cpp
+++ BlackJack/NotSoStupidStrategy.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 #include <vector>
 #define Pass -1
+#define EASY 1
+#define DECK 2
+#define HARD 3
+#define QUIT -1
+#define DETAILED 0
 
 static bool g(){
     Factory<Strategy, std::string,Strategy*(*)()>::getInstance()->addCreator("NotSoStupid", createNotSoStupidStrategy);
@@ -15,32 +20,79 @@ Strategy *createNotSoStupidStrategy () {
     return new NotSoStupidStrategy;
 }
 
-int NotSoStupidStrategy::takeCard() {
-    return rand() % 10 + 1;
-}
-
-int NotSoStupidStrategy::play(int FirstCard, int FirstCardOfOpponent) {
-    std::vector<int> History;
-    History.push_back(FirstCard);
-    int Sum = FirstCard;
-    if (FirstCardOfOpponent == 10) {
-        for (int i = 0; i < History.size(); ++i) {
-            std::cout<<"History of not so stupid strategy: ";
-            std::cout<<History[i]<< " ";
-            std::cout<<std::endl;
-        }
-        return Pass;
-    }
-    int CurrentCard;
-    while (Sum <= 18) {
-        CurrentCard = takeCard();
-        History.push_back(CurrentCard);
-        Sum += CurrentCard;
-    }
-    std::cout<<"History of not so stupid strategy: ";
+int NotSoStupidStrategy::details(std::vector<int> History, int CurrentCard) {
+    std::cout<<"Details of not so stupid strategy:"<<std::endl;
+    std::cout<<"New card - "<<CurrentCard<<std::endl;
+    std::cout<<"Cards of not so stupid strategy strategy now: ";
     for (int i = 0; i < History.size(); ++i) {
         std::cout<<History[i]<< " ";
     }
-    std::cout<<std::endl;
+    if (getchar() == 113) {
+        return QUIT;
+    }
+}
+
+int NotSoStupidStrategy::takeCard(int CardMode, int NumberOfDecks) {
+    if (CardMode == EASY) {
+        return rand() % 10 + 1;
+    }
+    if (CardMode == DECK) {
+        std::vector<int> Deck;
+        int Card = 1;
+        for (int i = 0; i < 53; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                Deck.push_back(Card);
+            }
+            if (Card < 10)
+                Card++;
+        }
+        int index = rand() % 52 + 1;
+        return Deck[index];
+    }
+    if (CardMode == HARD) {
+        std::vector<int> Deck;
+        int Card = 1;
+        for (int i = 0; i < NumberOfDecks; ++i) {
+            for (int j = 0; j < 53; ++j) {
+                for (int k = 0; k < 4; ++k) {
+                    Deck.push_back(Card);
+                }
+                if (Card < 10)
+                    Card++;
+            }
+        }
+        int index = rand() % (52 * NumberOfDecks) + 1;
+        return Deck[index];
+    }
+}
+
+int NotSoStupidStrategy::play(int DealerCard, int Mode, int NumberOfDecks, int GameMode) {
+    std::vector<int> History;
+    History.push_back(DealerCard);
+    int Sum = DealerCard;
+    int CurrentCard = takeCard(Mode, NumberOfDecks);
+    History.push_back(CurrentCard);
+    Sum += CurrentCard;
+    CurrentCard = takeCard(Mode, NumberOfDecks);
+    History.push_back(CurrentCard);
+    Sum += CurrentCard;
+    if (GameMode == DETAILED) {
+        if(details(History, CurrentCard) == QUIT) {
+            return QUIT;
+        }
+    }
+    if (DealerCard == 10) {
+        return Pass;
+    }
+    while (Sum <= 18) {
+        CurrentCard = takeCard(Mode, NumberOfDecks);
+        History.push_back(CurrentCard);
+        Sum += CurrentCard;
+        if (GameMode == DETAILED) {
+            if(details(History, CurrentCard) == QUIT) {
+                return QUIT;
+            }
+        }
+    }
     return Sum;
 }
