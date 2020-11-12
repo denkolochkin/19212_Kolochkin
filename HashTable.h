a69a1efd22f4a7e32b1686475574efc90ebdacb8
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-11-13 00:36:24 +0700

    Update HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -4,44 +4,27 @@
 
 typedef std::string Key;
 
-template<typename T>
-T* begin(std::pair<T *, T *> const &p) { return p.first; }
-
-template<typename T>
-T* end(std::pair<T *, T *> const &p) { return p.second; }
-
-
-struct Value {
-    unsigned age;
-    unsigned weight;
-    Value(unsigned age_, unsigned weight_): age(age_), weight(weight_){};
-};
-
-bool operator==(const Value &a, const Value &b);
-
-struct Node {
-    Key key;
-    Value value;
-    Node *next;
-    Node(Key key_, Value value_, Node* next_) :key(key_), value(value_), next(next_){};
-};
-
-
-class HashTable {
+class HashTable{
 public:
+    struct Value {
+        unsigned age;
+        unsigned weight;
+        Value(unsigned age_, unsigned weight_): age(age_), weight(weight_){};
+    };
+
     HashTable();
     ~HashTable();
     HashTable(const HashTable &b);
 
     void swap(HashTable &b);
 
-    HashTable &operator=(HashTable &b);
+    HashTable &operator=(const HashTable &b);
 
     void clear() ;
 
     bool erase(const Key &k) ;
 
-    HashTable& resize_table(const Key &k, const Value &value);
+    void resize_table(const Key &k, const Value &value);
 
     bool insert(const Key &k, const Value &value);
 
@@ -63,7 +46,15 @@ public:
 
     int get_hash(const std::string &key) const;
 
+    friend bool operator==(const HashTable::Value &a, const HashTable::Value &b);
+    
 private:
+    struct Node{
+        Key key;
+        Value value;
+        Node *next;
+        Node(Key key_, Value value_, Node* next_) :key(key_), value(value_), next(next_){};
+    };
     Node** nodes = new Node*;
     int table_size;
     int current_table_size;
