ddd0c9705e30978eb221cb117bd40fc448edceb3
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-11-21 14:33:27 +0700

    Update BlackJackTests.cpp

--- BlackJack/BlackJackTests.cpp
+++ BlackJack/BlackJackTests.cpp
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
-#include "../main.cpp"
-#define Pass -1
+#include "BlackJack.h"
+#define Pass -2
 #define EASY 1
 #define DECK 2
 #define HARD 3
@@ -11,7 +11,6 @@
 
 namespace {
     class BlackJackTest : public ::testing::Test {};
-
     TEST_F(BlackJackTest, fast) {
         srand(time(0));
         int CardMode = EASY;
@@ -20,30 +19,73 @@ namespace {
         std::string first = "Base";
         std::string second = "Trivial";
         fast(first, second, CardMode, NumberOfDecks, GameMode);
+        first = "Base";
+        second = "Neat";
+        fast(first, second, CardMode, NumberOfDecks, GameMode);
+        first = "Trivial";
+        second = "NotSoStupid";
+        fast(first, second, CardMode, NumberOfDecks, GameMode);
     }
 
     TEST_F(BlackJackTest, fast_deck) {
         srand(time(0));
         int CardMode = DECK;
-        int GameMode = DETAILED;
+        int GameMode = FAST;
         int NumberOfDecks = 2;
         std::string first = "Stupid";
         std::string second = "Hard";
         fast(first, second, CardMode, NumberOfDecks, GameMode);
+        first = "Base";
+        second = "Neat";
+        fast(first, second, CardMode, NumberOfDecks, GameMode);
+        first = "Trivial";
+        second = "NotSoStupid";
+        fast(first, second, CardMode, NumberOfDecks, GameMode);
     }
 
-    TEST_F(BlackJackTest, tournament) {
+    TEST_F(BlackJackTest, hard) {
         srand(time(0));
         int CardMode = HARD;
+        int GameMode = FAST;
+        int NumberOfDecks = 2;
+        std::string first = "Stupid";
+        std::string second = "Hard";
+        fast(first, second, CardMode, NumberOfDecks, GameMode);
+        first = "Base";
+        second = "Neat";
+        fast(first, second, CardMode, NumberOfDecks, GameMode);
+        first = "Trivial";
+        second = "NotSoStupid";
+        fast(first, second, CardMode, NumberOfDecks, GameMode);
+    }
+
+    TEST_F(BlackJackTest, tournament_for_3) {
+        srand(time(0));
+        int CardMode = DECK;
+        int GameMode = DETAILED;
+        int NumberOfDecks = 2;
+        std::vector<std::string> Players;
+        Players.push_back("Trivial");
+        Players.push_back("NotSoStupid");
+        Players.push_back("Neat");
+        Tournament(Players, CardMode, NumberOfDecks, GameMode);
+        GameMode = DETAILED;
+        Players.push_back("Base");
+        Players.push_back("Stupid");
+        Players.push_back("Hard");
+        Tournament(Players, CardMode, NumberOfDecks, GameMode);
+    }
+
+    TEST_F(BlackJackTest, tournament_for_4) {
+        srand(time(0));
+        int CardMode = DECK;
         int GameMode = TOURNAMENT;
         int NumberOfDecks = 2;
         std::vector<std::string> Players;
-        std::string first = "NotSoStupid";
-        std::string second = "Neat";
-        std::string third = "Base";
-        Players.push_back(first);
-        Players.push_back(second);
-        Players.push_back(third);
+        Players.push_back("Trivial");
+        Players.push_back("NotSoStupid");
+        Players.push_back("Neat");
+        Players.push_back("Stupid");
         Tournament(Players, CardMode, NumberOfDecks, GameMode);
     }
 }
