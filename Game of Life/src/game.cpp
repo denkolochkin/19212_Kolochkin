70b1d84725fed1a4495ad38378da63a0af641180
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-12-04 16:01:00 +0700

    Update game.cpp

--- Game of Life/src/game.cpp
+++ Game of Life/src/game.cpp
@@ -1,101 +1,93 @@
 #include <QMessageBox>
+#include <iostream>
 #include <QTimer>
 #include <QMouseEvent>
 #include <QDebug>
 #include <QRectF>
 #include <QPainter>
 #include <qmath.h>
+#include<vector>
 #include "game.h"
 
-GameWidget::GameWidget(QWidget *parent) : QWidget(parent), canvasSize(10) {
-    cellsColor = "#000";
-    gridColor = "#000";
-    gridColor.setAlpha(30);
-    canvas = new bool[(canvasSize + 1) * (canvasSize + 1)];
-    memset(canvas, 0, sizeof(bool) * (canvasSize + 1) * (canvasSize + 1));
+GameWidget::GameWidget(QWidget *parent) : QWidget(parent), canvasSize(50) {
+    mainColor = "#000";
+    //canvas = new bool[(canvasSize + 1) * (canvasSize + 1)];
+    std::vector<std::vector<bool>> canvas(canvasSize + 1, std::vector<bool>(canvasSize + 1));
+    //memset(canvas, false, sizeof(bool) * (canvasSize + 1) * (canvasSize + 1));
 }
 
 GameWidget::~GameWidget() {
-    delete [] canvas;
+    //delete [] canvas;
 }
 
 void GameWidget::clear() {
     for(int k = 1; k <= canvasSize; k++) {
         for(int j = 1; j <= canvasSize; j++) {
-            canvas[k * canvasSize + j] = 0;
+            canvas[k][j] = false;
         }
-    }
-    if (gridColor != "#000") {
-        gridColor = "#000";
-        gridColor.setAlpha(30);
     }
     update();
 }
 
 void GameWidget::paintEvent(QPaintEvent *) {
     QPainter p(this);
-    makeGrid(p);
-    makeCanvas(p);
+    paintGrid(p);
+    paintCanvas(p);
 }
 
 void GameWidget::pressMouseEvent(QMouseEvent* event) {
-    double Width = (double)width() / canvasSize;
-    double Height = (double)height() / canvasSize;
-    int k = floor(event->y() / Height) + 1;
-    int j = floor(event->x() / Width) + 1;
-    canvas[k * canvasSize + j] = !canvas[k * canvasSize + j];
+    double cellWidth = (double)width() / canvasSize;
+    double cellHeight = (double)height() / canvasSize;
+    int k = floor(event->y() / cellHeight) + 1;
+    int j = floor(event->x() / cellWidth) + 1;
+    canvas[k][j] = !canvas[k][j];
     update();
 }
 
 void GameWidget::mouseMoveEvent(QMouseEvent* event) {
-    double Width = (double)width() / canvasSize;
-    double Height = (double)height() / canvasSize;
-    int i = floor(event->y() / Height) + 1;
-    int j = floor(event->x() / Width) + 1;
-    int location = i * canvasSize + j;
-    if(canvas[location] == 0) {
-        canvas[location] = 1;
+    double cellWidth = (double)width() / canvasSize;
+    double cellHeight = (double)height() / canvasSize;
+    int k = floor(event->y() / cellHeight) + 1;
+    int j = floor(event->x() / cellWidth) + 1;
+    if(!canvas[k][j]) {
+        canvas [k][j] = !canvas[k][j];
         update();
     }
 }
 
-void GameWidget::makeGrid(QPainter& painter) {
+void GameWidget::paintGrid(QPainter& painter) {
     QRect borders(0, 0, width() - 1, height() - 1);
+    QColor gridColor = mainColor;
+    gridColor.setAlpha(100);
     painter.setPen(gridColor);
-    gridColor.setAlpha(30);
-
-    double Width = (double)width() / canvasSize;
-    for(int i = Width; i <= width(); i += Width) {
-        painter.drawLine(i, 0, i, height());
+    double cellWidth = (double)width() / canvasSize;
+    for(double k = cellWidth; k <= width(); k += cellWidth) {
+        painter.drawLine(k, 0, k, height());
     }
-    double Height = (double)height() / canvasSize;
-    for(int i = Height; i <= height(); i += Height) {
-        painter.drawLine(0, i, width(), i);
+    double cellHeight = (double)height() / canvasSize;
+    for(double k = cellHeight; k <= height(); k += cellHeight) {
+        painter.drawLine(0, k, width(), k);
     }
     painter.drawRect(borders);
 }
 
-void GameWidget::makeCanvas(QPainter& painter) {
+void GameWidget::paintCanvas(QPainter& painter) {
     double Width = (double)width() / canvasSize;
     double Height = (double)height() / canvasSize;
-    for(int i = 1; i <= canvasSize; i++) {
+    std::cout<<canvas[1][1]<<std::endl;
+    for(int k = 1; k <= canvasSize; k++) {
         for(int j = 1; j <= canvasSize; j++) {
-            if(canvas[i * canvasSize + j] == 1) {
+            if(canvas[k][j] == true) {
                 qreal left = (qreal)(Width * j - Width);
-                qreal top  = (qreal)(Height * i - Height);
+                qreal top  = (qreal)(Height * k - Height);
                 QRectF rect(left, top, (qreal)Width, (qreal)Height);
-                painter.fillRect(rect, QBrush(cellsColor));
+                painter.fillRect(rect, QBrush(mainColor));
             }
         }
     }
 }
 
-void GameWidget::setCellsColor(const QColor &color){
-    cellsColor = color;
-    update();
-}
-
-void GameWidget::selectGridColor(const QColor &color){
-    gridColor = color;
+void GameWidget::setMainColor(const QColor &color){
+    mainColor = color;
     update();
 }
