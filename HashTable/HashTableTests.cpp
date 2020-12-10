03b9656803d07d2fe1f969e51bacab1577bf6b6d
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-12-10 22:52:30 +0700

    Update HashTableTests.cpp

--- HashTable/HashTableTests.cpp
+++ HashTable/HashTableTests.cpp
@@ -16,29 +16,30 @@ namespace{
     }
 
     TEST_F(HashTableTest, brackets_false){
-        HashTable::Value value(19, 170);
-        HashTable::Value b = a["lorem"];
+        Value value(19, 170);
+        Value b = a["lorem"];
         EXPECT_FALSE(b == value);
     }
 
     TEST_F(HashTableTest, brackets_true){
-        HashTable::Value value(19, 170);
+        Value value(19, 170);
         Key key = "lorem";
         a.insert(key, value);
-        HashTable::Value b = a["lorem"];
+        Value b = a["lorem"];
         EXPECT_TRUE(b == value);
     }
 
     TEST_F(HashTableTest, erase){
-        HashTable::Value value(19, 170);
+        Value value(19, 170);
         Key key = "lorem";
         a.insert(key, value);
         a.erase(key);
         EXPECT_FALSE(a.contains(key));
+        EXPECT_FALSE(a.erase("asd"));
     }
 
     TEST_F(HashTableTest, new_erase){
-        HashTable::Value value(19, 170);
+        Value value(19, 170);
         Key key_1 = "qwe";
         Key key_2 = "ewq";
         a.insert(key_1, value);
@@ -53,18 +54,18 @@ namespace{
         Key key_3 = "ips";
         Key key_4 = "um";
         Key key_5 = "lorem";
-        a.insert(key_1, HashTable::Value(19, 170));
-        a.insert(key_2, HashTable::Value(19, 170));
-        a.insert(key_3, HashTable::Value(19, 170));
-        a.insert(key_4, HashTable::Value(19,170));
-        a.insert(key_5, HashTable::Value(19,170));
+        a.insert(key_1, Value(19, 170));
+        a.insert(key_2, Value(19, 170));
+        a.insert(key_3, Value(19, 170));
+        a.insert(key_4, Value(19,170));
+        a.insert(key_5, Value(19,170));
         const HashTable b = a;
-        EXPECT_EQ(b.at(key_1), HashTable::Value(19, 170));
+        EXPECT_EQ(b.at(key_1), Value(19, 170));
     }
 
     TEST_F(HashTableTest, at_false){
-        HashTable::Value value1(19, 170);
-        HashTable::Value value2(18, 120);
+        Value value1(19, 170);
+        Value value2(18, 120);
         Key key1 = "test";
         Key key2 = "lorem";
         a.insert(key1, value1);
@@ -74,7 +75,7 @@ namespace{
 
     TEST_F(HashTableTest, at){
         HashTable k;
-        HashTable::Value value(19, 170);
+        Value value(19, 170);
         Key key = "qwe";
         Key key1 = "ewq";
         k.insert(key, value);
@@ -85,15 +86,20 @@ namespace{
     TEST_F(HashTableTest, equal){
         HashTable g;
         HashTable j;
-        g.insert("qwe", HashTable::Value(1, 1));
-        g.insert("ewq", HashTable::Value(1, 1));
-        j.insert("qwe", HashTable::Value(1, 1));
-        j.insert("ewq", HashTable::Value(1, 1));
+        g.insert("qwe", Value(1, 1));
+        g.insert("ewq", Value(1, 1));
+        j.insert("qwe", Value(1, 1));
+        j.insert("ewq", Value(1, 1));
         EXPECT_TRUE(j == g);
     }
 
     TEST_F(HashTableTest, not_equal){
         HashTable c;
+        Value value(19, 175);
+        Key key = "qwe";
+        Key key1 = "ewq";
+        c.insert(key, value);
+        c.insert(key1, value);
         c = a;
         EXPECT_FALSE(a != c);
     }
@@ -101,7 +107,7 @@ namespace{
     TEST_F(HashTableTest, swap){
         HashTable a;
         HashTable b;
-        HashTable::Value value(1,1);
+        Value value(1,1);
         a.insert("som", value);
         b.insert("lorem", value);
         a.swap(b);
@@ -110,7 +116,7 @@ namespace{
 
     TEST_F(HashTableTest, resize){
         HashTable a;
-        HashTable::Value value(1,1);
+        Value value(1,1);
         a.insert("lor",value);
         a.insert("em",value);
         a.insert("ip",value);
