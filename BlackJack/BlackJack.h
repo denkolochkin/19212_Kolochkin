7471fdf382aa929a67303c3133b69a6526240c80
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-12-16 01:05:10 +0700

    Update BlackJack.h

--- BlackJack/BlackJack.h
+++ BlackJack/BlackJack.h
@@ -1,20 +1,27 @@
-#include <iostream>
 #include "Factory.h"
 #include "Strategy.h"
+
+#include <iostream>
 #include <vector>
-#define Pass -1
-#define EASY 1
-#define DECK 2
-#define HARD 3
-#define DETAILED 0
-#define TOURNAMENT 1
-#define FAST 4
-#define QUIT -1
 
-int ChooseWinner (int first, std::string firstName, int second, std::string secondName);
+enum modes {
+    Pass, EASY, DECK, HARD, QUIT, DETAILED, TOURNAMENT, FAST
+};
 
-std::unique_ptr<Strategy>& TournamentOfTwo (std::unique_ptr<Strategy>& first, std::string firstName, std::unique_ptr<Strategy>& second,std::string secondName, int Mode, int NumberOfDecks, int GameMode);
+class BlackJack {
+public:
+    BlackJack();
+    ~BlackJack();
+    static int ChooseWinner(int first, std::string firstName, int second, std::string secondName);
 
-void fast (std::string first, std::string second, int CardMode, int NumberOfDecks, int GameMode);
+    static std::unique_ptr<Strategy>& TournamentOfTwo(
+                                    std::unique_ptr<Strategy> &first, std::string firstName,
+                                    std::unique_ptr<Strategy> &second, std::string secondName,
+                                    int Mode, int NumberOfDecks, int GameMode
+                                    );
 
-void Tournament(std::vector<std::string> Players , int CardMode, int NumberOfDecks, int GameMode);
+    static void fast(std::string first, std::string second, int CardMode, int NumberOfDecks, int GameMode);
+
+    static void Tournament(std::vector<std::string> Players, int CardMode, int NumberOfDecks, int GameMode);
+
+};
