03df4fa94c6b77d8695ca24a9df540f4b947a128
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-12-16 01:14:10 +0700

    Update TrivialStrategy.h

--- BlackJack/TrivialStrategy.h
+++ BlackJack/TrivialStrategy.h
@@ -1,13 +1,13 @@
 #include "Strategy.h"
 
+enum modes {
+    Pass, EASY, DECK, HARD, QUIT, DETAILED, TOURNAMENT, FAST
+};
+
 class TrivialStrategy : public Strategy {
 public:
     ~TrivialStrategy(){}
-    int takeCard(int, int) override;
-    int play(int, int, int, int) override;
+    int takeCard(int CardMode, int NumberOfDecks) override;
+    int play(int DealerCard, int Mode, int NumberOfDecks, int GameMode) override;
     int details(std::vector<int> History, int CurrentCard) override;
-private:
-
 };
-
-Strategy* createTrivialStrategy ();
