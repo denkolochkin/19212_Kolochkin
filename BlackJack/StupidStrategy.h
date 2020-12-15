e3a1557f2bf0deed01c07d838839645099094daa
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-12-16 01:11:57 +0700

    Update StupidStrategy.h

--- BlackJack/StupidStrategy.h
+++ BlackJack/StupidStrategy.h
@@ -1,13 +1,14 @@
 #include "Strategy.h"
 
+enum modes {
+    Pass, EASY, DECK, HARD, QUIT, DETAILED, TOURNAMENT, FAST
+};
+
 class StupidStrategy : public Strategy {
 public:
     ~StupidStrategy(){}
-    int takeCard(int, int) override;
-    int play(int, int, int, int) override;
+    int takeCard(int CardMode, int NumberOfDecks) override;
+    int play(int DealerCard, int Mode, int NumberOfDecks, int GameMode) override;
     int details(std::vector<int> History, int CurrentCard) override;
-private:
 
 };
-
-Strategy* createStupidStrategy ();
