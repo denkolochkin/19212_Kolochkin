32bca830fc16689102644e4f38e8a3bcb541a255
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-12-16 01:10:45 +0700

    Update NotSoStupidStrategy.h

--- BlackJack/NotSoStupidStrategy.h
+++ BlackJack/NotSoStupidStrategy.h
@@ -1,13 +1,13 @@
 #include "Strategy.h"
 
-class NotSoStupidStrategy : public Strategy {
-public:
-    ~NotSoStupidStrategy(){}
-    int takeCard(int, int) override;
-    int play(int, int, int, int) override;
-    int details(std::vector<int> History, int CurrentCard) override;
-private:
-
+enum modes {
+    Pass, EASY, DECK, HARD, QUIT, DETAILED, TOURNAMENT, FAST
 };
 
-Strategy* createNotSoStupidStrategy ();
+class NeatStrategy : public Strategy {
+public:
+    ~NeatStrategy(){}
+    int takeCard(int CardMode, int NumberOfDecks) override;
+    int play(int DealerCard, int Mode, int NumberOfDecks, int GameMode) override;
+    int details(std::vector<int> History, int CurrentCard) override;
+};
