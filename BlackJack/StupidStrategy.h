ec3dcb072cf93d880935c2f28ac6278fa1e506a4
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-12-16 01:12:54 +0700

    Update StupidStrategy.h

--- BlackJack/StupidStrategy.h
+++ BlackJack/StupidStrategy.h
@@ -10,5 +10,4 @@ public:
     int takeCard(int CardMode, int NumberOfDecks) override;
     int play(int DealerCard, int Mode, int NumberOfDecks, int GameMode) override;
     int details(std::vector<int> History, int CurrentCard) override;
-
 };
