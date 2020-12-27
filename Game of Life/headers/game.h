455f588384766208d3abc452f55b69d93765b253
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-12-27 23:18:39 +0700

    Update game.h

--- Game of Life/headers/game.h
+++ Game of Life/headers/game.h
@@ -1,34 +1,36 @@
-#ifndef GAMEWIDGET_H
-#define GAMEWIDGET_H
+#pragma once
 
-#include <QColor>
-#include <QWidget>
+#include <vector>
+#include <algorithm>
+#include <string>
 
-class GameWidget : public QWidget {
-    Q_OBJECT
+class GameWidget {
 public:
-    explicit GameWidget(QWidget *parent = 0);
-    ~GameWidget();
-
-protected:
-    void paintEvent(QPaintEvent *);
-    void pressMouseEvent(QMouseEvent *e);
-    void mouseMoveEvent(QMouseEvent *e);
-
-public slots:
-    void clear(); // clear
-    void setMainColor(const QColor &color); // set color of the cells
-
-private slots:
-    void paintGrid(QPainter &painter);
-    void paintCanvas(QPainter &painter);
-
+    GameWidget();
+    size_t getCanvasSizeX();
+    size_t getCanvasSizeY();
+    bool newGeneration();
+    void clear();
+    void getB(std::vector<int> b);
+    void getS(std::vector<int> s);
+    void mouseEvent(size_t k, size_t j);
+    void moveEvent(size_t k, size_t j);
+    bool checkCell(size_t k, size_t j);
+    void setSizeX(size_t x);
+    void setSizeY(size_t y);
+    void writeRule(std::string &s);
+    void writeSizes(std::string &s);
+    void writeCanvas(std::string &canvas);
+    bool processFile(const std::string s);
 private:
-    QColor mainColor;
-    //bool* canvas; // map
-
-    int canvasSize;
+    bool isAlive(size_t k, size_t j);
+    void resetCanvas();
+    void change(std::string &s, char c, size_t count);
     std::vector<std::vector<bool>> canvas;
+    std::vector<std::vector<bool>> nextCanvas;
+    int generations = -1;
+    size_t canvasSizeX;
+    size_t canvasSizeY;
+    std::vector<int> B;
+    std::vector<int> S;
 };
-
-#endif // GAMEWIDGET_H
