52eb383726a3a31d8c23a19f89e5e778a0a6c543
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-12-27 23:20:14 +0700

    Update window.h

--- Game of Life/headers/window.h
+++ Game of Life/headers/window.h
@@ -1,25 +1,47 @@
+#pragma once
+
+#include "painter.h"
+#include "rulesdialog.h"
+#include "sizedialog.h"
+
 #include <QMainWindow>
-#include <QColor>
-#include "game.h"
+#include <QWidget>
+#include <QColorDialog>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QString>
+#include <QTextStream>
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
-    
+
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 public slots:
-    void selectCellsColor();
-    void selectGridColor();
+    void selectMasterColor();
+    void selectRules();
+    void selectSize();
+
+private slots:
+    void on_btnSave_clicked();
+    void on_btnOpen_clicked();
 
 private:
     Ui::MainWindow *ui;
     QColor currentColor;
-    QColor gridColor;
-    GameWidget* game;
+    std::vector<int> B;
+    std::vector<int> S;
+    size_t x;
+    size_t y;
+    size_t canvasSizeX;
+    size_t canvasSizeY;
+    PainterWidget* painter;
+    RulesDialog* rules;
+    SizeDialog* size;
 };
