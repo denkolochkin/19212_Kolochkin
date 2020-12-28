e16dc0213edeace9b9bd71d89020c659e2a5da52
Denis <70904339+denkolochkin@users.noreply.github.com>
2020-12-28 16:38:52 +0700

    Update BlackJack.h

--- BlackJack/BlackJack.h
+++ BlackJack/BlackJack.h
@@ -6,24 +6,21 @@
 #include <iostream>
 #include <vector>
 
-enum modes {
-    Pass, EASY, DECK, HARD, QUIT, DETAILED, TOURNAMENT, FAST
-};
-
 class BlackJack {
 public:
     BlackJack();
     ~BlackJack();
-    static int ChooseWinner(int first, std::string firstName, int second, std::string secondName);
+    int ChooseWinner(int first, std::string firstName, int second, std::string secondName);
 
-    static std::unique_ptr<Strategy>& TournamentOfTwo(
+    std::unique_ptr<Strategy>& TournamentOfTwo(
                                     std::unique_ptr<Strategy> &first, std::string firstName,
                                     std::unique_ptr<Strategy> &second, std::string secondName,
                                     int Mode, int NumberOfDecks, int GameMode
                                     );
 
-    static void fast(std::string first, std::string second, int CardMode, int NumberOfDecks, int GameMode);
+    void fast(std::string first, std::string second, int CardMode, int NumberOfDecks, int GameMode);
 
-    static void Tournament(std::vector<std::string> Players, int CardMode, int NumberOfDecks, int GameMode);
+    void Tournament(std::vector<std::string> Players, int CardMode, int NumberOfDecks, int GameMode);
 
+    std::vector<std::string> currentPlayers; //for test
 };
