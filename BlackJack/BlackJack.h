89747653df7067d640362097d0dfa606868c787e
Denis <70904339+denkolochkin@users.noreply.github.com>
2021-01-02 15:14:10 +0700

    Update BlackJack.h

--- BlackJack/BlackJack.h
+++ BlackJack/BlackJack.h
@@ -1,26 +1,26 @@
 #pragma once
 
-#include "Factory.h"
 #include "Strategy.h"
 
-#include <iostream>
 #include <vector>
 
 class BlackJack {
 public:
     BlackJack();
     ~BlackJack();
-    int ChooseWinner(int first, std::string firstName, int second, std::string secondName);
 
     std::unique_ptr<Strategy>& TournamentOfTwo(
                                     std::unique_ptr<Strategy> &first, std::string firstName,
-                                    std::unique_ptr<Strategy> &second, std::string secondName,
-                                    int Mode, int NumberOfDecks, int GameMode
+                                    std::unique_ptr<Strategy> &second, std::string secondName
                                     );
 
-    void fast(std::string first, std::string second, int CardMode, int NumberOfDecks, int GameMode);
+    void fast(std::vector<std::string> Players);
 
-    void Tournament(std::vector<std::string> Players, int CardMode, int NumberOfDecks, int GameMode);
+    void Tournament(std::vector<std::string> Players);
 
-    std::vector<std::string> currentPlayers; //for test
+    std::vector<std::string> currentPlayers;
+    int CardMode;
+    int NumberOfDecks;
+    int GameMode;
+    std::string WinnerName;
 };
