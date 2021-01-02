e64a18fa20f766f62ee670be15c8b115c4521654
Denis <70904339+denkolochkin@users.noreply.github.com>
2021-01-02 17:00:31 +0700

    Update rulesdialog.h

--- Game of Life/headers/rulesdialog.h
+++ Game of Life/headers/rulesdialog.h
@@ -16,49 +16,28 @@ public:
     std::vector<int> S;
     std::vector<int> countB;
     std::vector<int> countS;
-
-
+    
 private slots:
-
     void changeRulesB(size_t index);
-
     void changeRulesS(size_t index);
-
     void on_btnOK_clicked();
-
     void on_checkBox_B1_stateChanged();
-
     void on_checkBox_B2_stateChanged();
-
     void on_checkBox_B3_stateChanged();
-
     void on_checkBox_B4_stateChanged();
-
     void on_checkBox_B5_stateChanged();
-
     void on_checkBox_B6_stateChanged();
-
     void on_checkBox_B7_stateChanged();
-
     void on_checkBox_B8_stateChanged();
-
     void on_checkBox_S1_stateChanged();
-
     void on_checkBox_S2_stateChanged();
-
     void on_checkBox_S3_stateChanged();
-
     void on_checkBox_S4_stateChanged();
-
     void on_checkBox_S5_stateChanged();
-
     void on_checkBox_S6_stateChanged();
-
     void on_checkBox_S7_stateChanged();
-
     void on_checkBox_S8_stateChanged();
-
+    
 private:
     Ui::RulesDialog *ui;
 };
-
