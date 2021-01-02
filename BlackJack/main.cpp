9a1da6e0d99615099918dfc6e4c4369c6cd3911d
Denis <70904339+denkolochkin@users.noreply.github.com>
2021-01-02 15:28:25 +0700

    Update main.cpp

--- BlackJack/main.cpp
+++ BlackJack/main.cpp
@@ -1,33 +1,34 @@
 #include "BlackJack.h"
+#include <vector>
 
 int main (int argc, char *argv[]) {
-    int CardMode = EASY;
-    int NumberOfDecks = 2;
-    int GameMode = 0;
+    BlackJack game;
+    game.CardMode = EASY;
+    game.GameMode = 0;
+    game.NumberOfDecks = 2;
     srand(time(0));
     if (argv[1][0] == 100) {
-        GameMode = DETAILED;
+        game.GameMode = DETAILED;
         std::vector<std::string> Players;
         for (int i = 2; i <= argc - 1; ++i) {
             Players.push_back(argv[i]);
         }
-        Tournament(Players, CardMode, NumberOfDecks, GameMode);
+        game.Tournament(Players);
     }
     if (argv[1][0] == 116) {
-        GameMode = TOURNAMENT;
+        game.GameMode = TOURNAMENT;
         std::vector<std::string> Players;
-        std::string first;
-        std::string second;
         for (int i = 2; i <= argc - 1; ++i) {
             Players.push_back(argv[i]);
         }
-        Tournament(Players, CardMode, NumberOfDecks, GameMode);
+        game.Tournament(Players);
     }
     if (argv[1][0] == 102) {
-        GameMode = FAST;
-        std::string first = argv[2];
-        std::string second = argv[3];
-        fast(first, second, CardMode, NumberOfDecks, GameMode);
+        game.GameMode = FAST;
+        std::vector<std::string> Players;
+        Players.push_back(argv[2]);
+        Players.push_back(argv[3]);
+        game.fast(Players);
     }
     return 0;
 }
