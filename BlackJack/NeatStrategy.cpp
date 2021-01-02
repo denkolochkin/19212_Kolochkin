1c2555fcff223a329b88b14e3bef226dd9a218e2
Denis <70904339+denkolochkin@users.noreply.github.com>
2021-01-02 15:23:53 +0700

    Update NeatStrategy.cpp

--- BlackJack/NeatStrategy.cpp
+++ BlackJack/NeatStrategy.cpp
@@ -12,7 +12,7 @@ static bool g(){
     return true;
 }
 
-static bool b=g();
+static bool b = g();
 
 int NeatStrategy::details(std::vector<int> History, int CurrentCard) {
     std::cout<<"Details of neat strategy:"<<std::endl;
