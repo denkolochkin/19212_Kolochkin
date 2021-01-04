bf084475613f6df4f0710222bbd0b7e23b47a1e3
Denis <70904339+denkolochkin@users.noreply.github.com>
2021-01-04 17:23:32 +0700

    Update HashTableTests.cpp

--- HashTable/HashTableTests.cpp
+++ HashTable/HashTableTests.cpp
@@ -4,13 +4,13 @@
 namespace{
     class HashTableTest : public ::testing::Test {
     protected:
-        HashTable a;
+        HashTable<std::string, Value> a;
     };
 
     TEST_F(HashTableTest, empty){
-        HashTable b;
+        HashTable<std::string, Value> b;
         Value value(19, 170);
-        Key key = "lorem";
+        std::string key = "lorem";
         b.insert(key, value);
         b.clear();
         EXPECT_TRUE(b.empty());
@@ -18,13 +18,13 @@ namespace{
 
     TEST_F(HashTableTest, brackets_false){
         Value value(19, 170);
-        Value b = a["lorem"];
+        Value b = a["lor"];
         EXPECT_FALSE(b == value);
     }
 
     TEST_F(HashTableTest, brackets_true){
         Value value(19, 170);
-        Key key = "lorem";
+        std::string key = "lorem";
         a.insert(key, value);
         Value b = a["lorem"];
         EXPECT_TRUE(b == value);
@@ -32,7 +32,7 @@ namespace{
 
     TEST_F(HashTableTest, erase){
         Value value(19, 170);
-        Key key = "lorem";
+        std::string key = "lorem";
         a.insert(key, value);
         a.erase(key);
         EXPECT_FALSE(a.contains(key));
@@ -40,53 +40,54 @@ namespace{
     }
 
     TEST_F(HashTableTest, new_erase){
-        Value value(19, 170);
-        Key key_1 = "qwe";
-        Key key_2 = "ewq";
-        a.insert(key_1, value);
-        a.insert(key_2, value);
+        Value value_1(19, 170);
+        Value value_2(20, 160);
+        std::string key_1 = "qwe";
+        std::string key_2 = "ewq";
+        a.insert(key_1, value_1);
+        a.insert(key_2, value_2);
         a.erase(key_2);
         EXPECT_FALSE(a.contains(key_2));
     }
 
     TEST_F(HashTableTest, const_at){
-        Key key_1 = "lor";
-        Key key_2 = "em";
-        Key key_3 = "ips";
-        Key key_4 = "um";
-        Key key_5 = "lorem";
+        std::string key_1 = "lor";
+        std::string key_2 = "em";
+        std::string key_3 = "ips";
+        std::string key_4 = "um";
+        std::string key_5 = "lorem";
         a.insert(key_1, Value(19, 170));
         a.insert(key_2, Value(19, 170));
         a.insert(key_3, Value(19, 170));
         a.insert(key_4, Value(19,170));
         a.insert(key_5, Value(19,170));
-        const HashTable b = a;
+        const HashTable<std::string, Value> b = a;
         EXPECT_EQ(b.at(key_1), Value(19, 170));
     }
 
     TEST_F(HashTableTest, at_false){
         Value value1(19, 170);
         Value value2(18, 120);
-        Key key1 = "test";
-        Key key2 = "lorem";
+        std::string key1 = "test";
+        std::string key2 = "lorem";
         a.insert(key1, value1);
         a.insert(key2, value2);
         EXPECT_FALSE(a.at(key1) == value2);
     }
 
     TEST_F(HashTableTest, at){
-        HashTable k;
+        HashTable<std::string, Value> k;
         Value value(19, 170);
-        Key key = "qwe";
-        Key key1 = "ewq";
+        std::string key = "qwe";
+        std::string key1 = "ewq";
         k.insert(key, value);
         k.insert(key1, value);
         EXPECT_EQ(k.at(key1), value);
     }
 
     TEST_F(HashTableTest, equal){
-        HashTable g;
-        HashTable j;
+        HashTable<std::string, Value> g;
+        HashTable<std::string, Value> j;
         g.insert("qwe", Value(1, 1));
         g.insert("ewq", Value(1, 1));
         j.insert("qwe", Value(1, 1));
@@ -95,10 +96,10 @@ namespace{
     }
 
     TEST_F(HashTableTest, not_equal){
-        HashTable c;
+        HashTable<std::string, Value> c;
         Value value(19, 175);
-        Key key = "qwe";
-        Key key1 = "ewq";
+        std::string key = "qwe";
+        std::string key1 = "ewq";
         c.insert(key, value);
         c.insert(key1, value);
         c = a;
@@ -106,8 +107,8 @@ namespace{
     }
 
     TEST_F(HashTableTest, swap){
-        HashTable a;
-        HashTable b;
+        HashTable<std::string, Value> a;
+        HashTable<std::string, Value> b;
         Value value(1,1);
         a.insert("som", value);
         b.insert("lorem", value);
@@ -116,7 +117,7 @@ namespace{
     }
 
     TEST_F(HashTableTest, resize){
-        HashTable a;
+        HashTable<std::string, Value> a;
         Value value(1,1);
         a.insert("lor",value);
         a.insert("em",value);
@@ -128,7 +129,7 @@ namespace{
         EXPECT_TRUE(a.size() == 5);
     }
     TEST_F(HashTableTest, new_resize){
-        HashTable a;
+        HashTable<std::string, Value> a;
         Value value(1,1);
         a.insert("lor", value);
         a.insert("em", value);
@@ -142,5 +143,55 @@ namespace{
         EXPECT_TRUE(a.contains("mus"));
         EXPECT_TRUE(a.size() == 7);
     }
-    
+
+    TEST_F(HashTableTest, operator_equal) {
+        HashTable<std::string, Value> a;
+        Value value(1,1);
+        a.insert("lor", value);
+        a.insert("em", value);
+        a.insert("ip", value);
+        a.insert("sum", value);
+        a.insert("lorem", value);
+        a.insert("mor", value);
+        a.insert("rom", value);
+        HashTable<std::string, Value> b;
+        b = a;
+        EXPECT_TRUE(b.contains("lor") == b.contains("em") == b.contains("sum"));
+        EXPECT_TRUE(b.contains("ip") == b.contains("lorem") == b.contains("mor"));
+        EXPECT_TRUE(b.contains("rom"));
+    }
+
+    TEST_F(HashTableTest, template_contains) {
+        HashTable<std::string, std::string> a;
+        std::string value = "Apple";
+        std::string key = "World";
+        a.insert(key, value);
+        EXPECT_TRUE(a.contains(key));
+        EXPECT_TRUE(a.size() == 1);
+    }
+
+    TEST_F(HashTableTest, template_at) {
+        HashTable<std::string, int> a;
+        std::string key = "World";
+        a.insert(key, 6);
+        EXPECT_TRUE(a.at(key) == 6);
+        EXPECT_TRUE(a.size() == 1);
+    }
+
+    TEST_F(HashTableTest, iterator_begin) {
+        HashTable<std::string, Value> a;
+        std::string key = "World";
+        a.insert(key, Value(19, 170));
+        EXPECT_TRUE((*a.begin()).key == key);
+    }
+
+    TEST_F(HashTableTest, iterator) {
+        HashTable<std::string, Value> a;
+        std::string key_1 = "World";
+        std::string key_2 = "lorem";
+        a.insert(key_1, Value(19, 170));
+        a.insert(key_2, Value(18, 190));
+        EXPECT_TRUE((*a.begin()).key == key_1);
+    }
+
 }
